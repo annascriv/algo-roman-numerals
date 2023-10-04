65c04165a0ec48169a757be6cbbaab246f5d0577
annascriv <scriven.anna@outlook.com>
2023-10-04 16:06:24 -0500

    updated object to include more specific roman numerals

--- romanNumerals.js
+++ romanNumerals.js
@@ -36,7 +36,30 @@ function toRomanLazy(num) {
 
 console.log(toRomanLazy(100))
 function toRoman(num) {
-  return "";
+  let output = "";
+  let romanNumeralToArabic = {
+    'M':1000,
+    'CM':900,
+    'D':500,
+    'CD':400,
+    'C':100,
+    'XC':90,
+    'L':50,
+    'XL':40,
+    'X':10,
+    'IX':9,
+    'V':5,
+    'IV':4,
+    'I':1
+      }   
+
+  for (let i of Object.keys(romanNumeralToArabic)) {
+    let ind = Math.floor(num/romanNumeralToArabic[i]);
+    num-=ind*romanNumeralToArabic[i];
+    output+=i.repeat(ind);
+  }
+
+  return output;
 }
 
 module.exports = { toRoman, toRomanLazy };
